a3900ae35489975df6fb778df548d29e2ea81a7f
Usuario <Usuario@DESKTOP-D1O15PC>
2022-06-27 02:11:31 -0600

    Cuarto avance

--- src/main/java/com/practica01/controller/indexcontroller.java
+++ src/main/java/com/practica01/controller/indexcontroller.java
@@ -1,11 +1,13 @@
 package com.practica01.controller;
 
+import com.practica01.domain.Cliente;
 import com.practica01.service.ClienteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -27,5 +29,29 @@ public class indexcontroller {
 //        model.addAttribute("clientes", clientes);
 
         return "index";
+    }
+    
+    @GetMapping("/nuevoEstado")
+    public String nuevoEstado(Cliente cliente){
+        return "modificarEstado";
+    }
+    
+    @PostMapping("/guardarEstado")
+    public String guardarEstado(Cliente cliente){
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/modificarEstado/{idEstado}")
+    public String modificarEstado(Cliente cliente, Model model){
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarEstado";
+    }
+    
+    @GetMapping("/eliminarEstado/{idEstado}")
+    public String eliminarEstado(Cliente cliente){
+        clienteService.delete(cliente);
+        return "redirect:/";
     }
 }
